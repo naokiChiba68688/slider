dde729aa470bd031bc24bf9c6230b9d2d8d58ee1
naoki kawamura <naokika@outlook.jp>
2022-04-04 10:31:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("hello world");
 function right_scroll(){
     var image_width = document.getElementsByClassName('slider_image');
     var slider_content = document.getElementById('slider_inner');
-    console.log(slider_content.left_scroll);
+    console.log(slider_content.scrollLeft);
 }
 function left_scroll(){
 
