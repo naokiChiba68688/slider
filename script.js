cbc42d1cbda8c35d84fd12071d9bb097e93116d0
naoki kawamura <naokika@outlook.jp>
2022-04-04 10:27:01 +0900

    commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+function right_scroll(){
+    var image_width = document.getElementsByClassName('slider_image');
+    var slider_content = document.getElementById('slider_inner');
+    console.log(slider_content.left_scroll);
+}
+function left_scroll(){
+
+}
