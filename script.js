3029398fe63f857cc60eea76f8f065769af76caa
naoki kawamura <naokika@outlook.jp>
2022-04-04 10:34:22 +0900

    commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function right_scroll(){
     var image_width = document.getElementsByClassName('slider_image');
     var slider_content = document.getElementById('slider_inner');
     console.log(slider_content.scrollLeft);
+    slider_content.scrollLeft = slider_content.scrollLeft + image_width;
+    console.log(slider_content.scrollLeft);
 }
 function left_scroll(){
 
